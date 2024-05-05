f5e1ce3b54b4f3185e092908fe4e733b53372cfa
Małgorzata Falecka <malgorzata.falecka.mail@gmail.com>
2024-05-05 14:41:08 +0200

    Zad2

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,19 @@
 import company.beans.Person;
 import company.exceptions.InvalidAgeException;
-
+import company.implementations.EmailMessenger;
+import company.interfaces.Messenger;
 public class Main {
     public static void main(String[] args) {
         try {
-            Person person = new Person("John", 30);
+            Person person = new Person("Jan", 25);
             System.out.println("Name: " + person.getName());
             System.out.println("Age: " + person.getAge());
         } catch (InvalidAgeException e) {
             System.out.println("Invalid age: " + e.getMessage());
         }
+        Messenger emailMessenger = new EmailMessenger();
+        emailMessenger.sendMessage("Hello, this is a test email!");
+
+
     }
 }
